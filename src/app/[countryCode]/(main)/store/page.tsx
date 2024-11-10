0f7020f481e989fbdb695c861518ed91097ebee9
Ira <ira@ira.nz>
2024-11-11 06:44:13 +1300

    Update site metadata

--- src/app/[countryCode]/(main)/store/page.tsx
+++ src/app/[countryCode]/(main)/store/page.tsx
@@ -4,8 +4,8 @@ import { SortOptions } from "@modules/store/components/refinement-list/sort-prod
 import StoreTemplate from "@modules/store/templates"
 
 export const metadata: Metadata = {
-  title: "Store",
-  description: "Explore all of our products.",
+  title: "Hauora Kai Karori by CoShop",
+  description: "Cheap, fresh, healthy produce for Karori",
 }
 
 type Params = {
