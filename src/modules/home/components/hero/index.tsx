bdcd6e274149ba01c1bf745f40c54e72a29e41b5
Ira <ira@ira.nz>
2024-11-11 05:02:41 +1300

    Add frontpage info

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -1,31 +1,32 @@
 import { Github } from "@medusajs/icons"
-import { Button, Heading } from "@medusajs/ui"
+import { Button, Heading, Text } from "@medusajs/ui"
 
 const Hero = () => {
   return (
-    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
-      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
+    <div className="h-[30vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
+      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-16 gap-4">
         <span>
           <Heading
             level="h1"
-            className="text-3xl leading-10 text-ui-fg-base font-normal"
+            className="text-2xl leading-8 text-ui-fg-base font-normal"
           >
-            Ecommerce Starter Template
+            Ordering Now Open
           </Heading>
           <Heading
-            level="h2"
-            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
+            level="h1"
+            className="text-2xl leading-8 text-ui-fg-base font-normal"
           >
-            Powered by Medusa and Next.js
+            For Thursday 14th November Pickup
           </Heading>
+          <Text>
+            8:30am-10am and 5pm-6pm from the Karori Community Centre
+          </Text>
         </span>
         <a
-          href="https://github.com/medusajs/nextjs-starter-medusa"
-          target="_blank"
+          href="https://karori.coshop.nz/nz/store"
         >
           <Button variant="secondary">
-            View on GitHub
-            <Github />
+            Order Here
           </Button>
         </a>
       </div>
