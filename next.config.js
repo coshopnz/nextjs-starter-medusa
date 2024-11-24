4c653fc31fcdff1dfe494246702f37381a64f5d3
Ira <ira@ira.nz>
2024-11-25 09:07:06 +1300

    remove invalid maximumCacheTTL

--- next.config.js
+++ next.config.js
@@ -10,7 +10,6 @@ const nextConfig = withStoreConfig({
   images: {
     formats: [],
     minimumCacheTTL: 60,
-    maximumCacheTTL: 300,
     // Add cache cleanup
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
