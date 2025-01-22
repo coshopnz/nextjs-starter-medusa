80a9acc842e836a5aa2eb14008a103720294f727
Ira <ira@ira.nz>
2025-01-23 09:48:15 +1300

    Update to Jan

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
             level="h1"
             className="text-2xl leading-8 text-ui-fg-base font-normal"
           >
-            For Thursday 12th December Pickup
+            For Thursday 30th January Pickup
           </Heading>
           <Text>
             8:30am-10am and 5pm-6pm from the Karori Community Centre
