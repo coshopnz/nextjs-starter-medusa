4288a9fcab2ae926d44f403c01cd6fab420096ec
Ira Bailey <ira.bailey@precisionfarming.co.nz>
2025-02-23 16:24:17 +1300

    add to cart from store page

--- src/modules/products/components/product-preview/index.tsx
+++ src/modules/products/components/product-preview/index.tsx
@@ -1,13 +1,11 @@
 import { Text } from "@medusajs/ui"
-
 import { ProductPreviewType } from "types/global"
-
 import { retrievePricedProductById } from "@lib/data"
 import { Region } from "@medusajs/medusa"
-import LocalizedClientLink from "@modules/common/components/localized-client-link"
 import Thumbnail from "../thumbnail"
 import { formatAmount } from "@lib/util/prices"
-
+import LocalizedClientLink from "@modules/common/components/localized-client-link"
+import ProductActions from "./product-actions"
 
 export default async function ProductPreview({
   productPreview,
@@ -25,10 +23,8 @@ export default async function ProductPreview({
 
   if (!pricedProduct) {
     return null
-  } 
+  }
 
-  // TODO refactor this function somewhere globally as our region will never change and it is duplicated a lot
-  // formats the product price to incl GST
   const getAmount = (amount: number | null | undefined) => {
     return formatAmount({
       amount: amount || 0,
@@ -41,23 +37,23 @@ export default async function ProductPreview({
     const priceInclGst = getAmount(pricedProduct.variants[0]?.original_price_incl_tax)
 
   return (
-    <LocalizedClientLink
-      href={`/products/${productPreview.handle}`}
-      className="group"
-    >
-      <div data-testid="product-wrapper">
-        <Thumbnail
-          thumbnail={productPreview.thumbnail}
-          size="full"
-          isFeatured={isFeatured}
-        />
-        <div className="flex justify-between mt-4 txt-compact-medium">
-          <Text className="text-ui-fg-subtle" data-testid="product-title">{productPreview.title}</Text>
-          <div className="flex items-center gap-x-2">
-          {priceInclGst}
+    <div className="group h-full">
+      <div data-testid="product-wrapper" className="flex flex-col h-full">
+        <LocalizedClientLink href={`/products/${productPreview.handle}`} className="flex-grow">
+          <Thumbnail
+            thumbnail={productPreview.thumbnail}
+            size="full"
+            isFeatured={isFeatured}
+          />
+        </LocalizedClientLink>
+        <div className="flex flex-col mt-2">
+          <div className="flex justify-between items-center mb-1">
+            <Text className="text-ui-fg-subtle font-medium" data-testid="product-title">{productPreview.title}</Text>
+            <Text className="text-ui-fg-subtle font-medium">{priceInclGst}</Text>
           </div>
+          <ProductActions product={pricedProduct} region={region} />
         </div>
       </div>
-    </LocalizedClientLink>
+    </div>
   )
 }
