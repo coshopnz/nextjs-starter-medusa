27629062636981eab018db59fd621671937419b8
Ira <ira@ira.nz>
2025-03-11 16:06:59 +1300

    Make grid more compact

--- src/modules/home/components/featured-products/product-rail/index.tsx
+++ src/modules/home/components/featured-products/product-rail/index.tsx
@@ -26,7 +26,7 @@ export default function ProductRail({
           View all
         </InteractiveLink>
       </div>
-      <ul className="grid grid-cols-2 small:grid-cols-3 gap-x-6 gap-y-24 small:gap-y-36">
+      <ul className="grid grid-cols-2 xsmall:grid-cols-3 small:grid-cols-4 gap-x-3 gap-y-16 small:gap-y-24">
         {products &&
           products.map((product) => (
             <li key={product.id}>
