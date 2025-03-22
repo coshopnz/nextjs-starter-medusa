7b42506ff6ab90b169844439204d152c91ec6826
Ira <ira@ira.nz>
2025-03-22 13:35:11 +1300

    Update to 27th March

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -27,7 +27,7 @@ const Hero = () => {
             level="h2"
             className="text-2xl leading-8 text-ui-fg-base font-normal mb-4"
           >
-            Ordering Now Open For Thursday 6th March Pickup
+            Ordering Now Open For Thursday 27th March Pickup
           </Heading>
           <Text className="text-lg">
             Choose your preferred pickup location below to get started
