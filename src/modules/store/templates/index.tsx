e1a824d038d36e5d173c2e0fb91e74aff9750a33
Ira <ira@ira.nz>
2025-04-12 14:27:05 +1200

    Add SBB order time

--- src/modules/store/templates/index.tsx
+++ src/modules/store/templates/index.tsx
@@ -49,14 +49,14 @@ const StoreTemplate = async ({
         
         return {
           handle,
-          title: handle === "produce" ? "Produce" : "Shelly Bay Baker Bread",
+          title: handle === "produce" ? "Produce" : "Shelly Bay Baker - Order by 11am Monday",
           products: productsWithOptions as (ProductPreviewType | PricedProduct)[],
         }
       } catch (error) {
         console.error(`Error fetching products for collection ${handle}:`, error)
         return {
           handle,
-          title: handle === "produce" ? "Produce" : "Shelly Bay Baker Bread",
+          title: handle === "produce" ? "Produce" : "Shelly Bay Baker - Order by 11am Monday",
           products: [],
         }
       }
