6db70b415e61f5b05ce7bf8864f5171592b84c24
Ira <ira@ira.nz>
2025-05-10 17:42:32 +1200

    May 15th

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -27,7 +27,7 @@ const Hero = () => {
             level="h2"
             className="text-2xl leading-8 text-ui-fg-base font-normal mb-4"
           >
-            Ordering Now Open For Thursday 8th of May Pickup
+            Ordering Now Open For Thursday 15th of May Pickup
           </Heading>
           <Text className="text-lg">
             Choose your preferred pickup location below to get started
