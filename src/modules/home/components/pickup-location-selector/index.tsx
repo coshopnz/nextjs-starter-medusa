62dc1485575b5f8ac338820f74630f795023f02e
Ira <ira@ira.nz>
2025-05-17 11:39:00 +1200

    Update to 22nd

--- src/modules/home/components/pickup-location-selector/index.tsx
+++ src/modules/home/components/pickup-location-selector/index.tsx
@@ -54,7 +54,7 @@ const PickupLocationSelector = ({ countryCode }: { countryCode: string }) => {
         </Heading>
         
         <Text className="text-center mb-6">
-          Select where you would like to pick up your order on Thursday 15th of May
+          Select where you would like to pick up your order on Thursday 22nd of May
         </Text>
 
         {/* Mobile-friendly scrollable container */}
